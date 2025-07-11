09e566ec980f371f0acd0412f298e6963cbbc9a0
jmctiernan1221 <jmctiernan1221@gmail.com>
2025-07-11 16:18:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config(); // Make sure you have a .env file with MONGO_URI
-const WaitlistEntry = require('./models/WaitlistEntry');
+const WaitlistEntry = require('./models/waitlistentry');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
